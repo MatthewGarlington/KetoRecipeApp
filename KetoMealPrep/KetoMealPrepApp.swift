880fde4fe8dd8386cd21f78260b35aa73b0a89b2
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-08 14:20:17 -0500

    Initial Commit

---
+++ KetoMealPrep/KetoMealPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  KetoMealPrepApp.swift
+//  KetoMealPrep
+//
+//  Created by Matthew Garlington on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct KetoMealPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
