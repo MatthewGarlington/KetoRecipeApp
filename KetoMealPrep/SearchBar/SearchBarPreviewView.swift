c18fe0a991354b8b0d00812fcadaf36971e7871a
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-19 13:20:33 -0500

    Feat: Added Search Bar Preview View that Shows Modal of Search Bar-mShows The Search Bar View in form of a sheet upon tap

---
+++ KetoMealPrep/SearchBar/SearchBarPreviewView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBarPreviewView.swift
+//  KetoMealPrep
+//
+//  Created by Matthew Garlington on 1/19/21.
+//
+
+import SwiftUI
+
+struct SearchBarPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBarPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarPreviewView()
+    }
+}
