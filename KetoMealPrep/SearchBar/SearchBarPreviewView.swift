532a1aab5e75faf2f2b8b3c14f211abcbb20f743
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-19 16:55:20 -0500

    Feat. Search Button Added
    Button Displays Modal of Search View

--- KetoMealPrep/SearchBar/SearchBarPreviewView.swift
+++ KetoMealPrep/SearchBar/SearchBarPreviewView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct SearchBarPreviewView: View {
+    
+    
+    @State private var showSearch: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            
+            VStack {
+                
+                //Search Bar View
+           
+                
+                
+                
+                
+                Button(action: {
+                    
+                    self.showSearch.toggle()
+                    
+                }, label: {
+                    
+                    
+                    
+                    
+                    ZStack(alignment: .leading) {
+                        
+                        
+                        Spacer()
+                            .frame(width: 400, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(15)
+                        
+                        
+                        
+                        
+                        Image(systemName: "magnifyingglass.circle.fill")
+                            .font(.system(size: 40))
+                            .foregroundColor(.secondary)
+                            .padding(.horizontal)
+                        
+                        
+                        
+                        
+                    }.opacity(0.4)
+                    
+                }).sheet(isPresented: $showSearch, content: {
+                    SearchBarView(isBreakFast1Favorited: FavoritedBreakfastStatus1.init(), isBreakFast2Favorited: FavoritedBreakfastStatus2.init(), isLunch3Favorited: FavoriteLunchStatus3.init(), isLunch4Favorited: FavoriteLunchStatus4.init(), isLunch2Favorited: FavoriteLunchStatus2.init(), isLunch1Favorited: FavoriteLunchStatus1.init(), isDinner1Favorited: FavoriteDinnerStatus1.init(), isDinner2Favorited: FavoriteDinnerStatus2.init(), isDinner3Favorited: FavoriteDinnerStatus3.init(), isFavorited: FavoritedStatus.init())
+                })
+            }
+        }
     }
 }
 
