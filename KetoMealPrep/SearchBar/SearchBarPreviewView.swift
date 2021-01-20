d397cf51ba98bbaaa173ebcb2beb0a8e0ae4de9a
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-20 06:25:36 -0500

    Style: Formatting changes to Search Bar Preview
    Opacity Increased and Frame Width size decrease to search bar preview view

--- KetoMealPrep/SearchBar/SearchBarPreviewView.swift
+++ KetoMealPrep/SearchBar/SearchBarPreviewView.swift
@@ -9,58 +9,72 @@ import SwiftUI
 
 struct SearchBarPreviewView: View {
     
+    struct Blur: UIViewRepresentable {
+        var style: UIBlurEffect.Style = .systemMaterial
+        func makeUIView(context: Context) -> UIVisualEffectView {
+            return UIVisualEffectView(effect: UIBlurEffect(style: style))
+        }
+        func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+            uiView.effect = UIBlurEffect(style: style)
+        }
+    }
+    
     
     @State private var showSearch: Bool = false
     
     var body: some View {
         
-        NavigationView {
+        
+        
+        VStack(alignment: .leading) {
             
-            VStack {
+            //Search Bar View
+            
+            
+            
+            
+            
+            Button(action: {
                 
-                //Search Bar View
-           
+                
+                self.showSearch.toggle()
+                
+                
+            }, label: {
                 
                 
                 
                 
-                Button(action: {
-                    
-                    self.showSearch.toggle()
-                    
-                }, label: {
+                ZStack(alignment: .leading) {
                     
                     
+                    Spacer()
+                        .frame(width: 100, height: 50)
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(15)
                     
                     
-                    ZStack(alignment: .leading) {
-                        
-                        
-                        Spacer()
-                            .frame(width: 400, height: 50)
-                            .background(Color.gray)
-                            .cornerRadius(15)
-                        
-                        
-                        
-                        
-                        Image(systemName: "magnifyingglass.circle.fill")
-                            .font(.system(size: 40))
-                            .foregroundColor(.secondary)
-                            .padding(.horizontal)
-                        
-                        
-                        
-                        
-                    }.opacity(0.4)
                     
-                }).sheet(isPresented: $showSearch, content: {
-                    SearchBarView(isBreakFast1Favorited: FavoritedBreakfastStatus1.init(), isBreakFast2Favorited: FavoritedBreakfastStatus2.init(), isLunch3Favorited: FavoriteLunchStatus3.init(), isLunch4Favorited: FavoriteLunchStatus4.init(), isLunch2Favorited: FavoriteLunchStatus2.init(), isLunch1Favorited: FavoriteLunchStatus1.init(), isDinner1Favorited: FavoriteDinnerStatus1.init(), isDinner2Favorited: FavoriteDinnerStatus2.init(), isDinner3Favorited: FavoriteDinnerStatus3.init(), isFavorited: FavoritedStatus.init())
-                })
-            }
+                    
+                    Image(systemName: "magnifyingglass")
+                        .font(.system(size: 30))
+                        .foregroundColor(.secondary)
+                        .padding(.horizontal)
+                    
+                    
+                    
+                    
+                }.opacity(0.8)
+                
+                
+                
+            }).sheet(isPresented: $showSearch, content: {
+                SearchBarView(isBreakFast1Favorited: FavoritedBreakfastStatus1.init(), isBreakFast2Favorited: FavoritedBreakfastStatus2.init(), isLunch3Favorited: FavoriteLunchStatus3.init(), isLunch4Favorited: FavoriteLunchStatus4.init(), isLunch2Favorited: FavoriteLunchStatus2.init(), isLunch1Favorited: FavoriteLunchStatus1.init(), isDinner1Favorited: FavoriteDinnerStatus1.init(), isDinner2Favorited: FavoriteDinnerStatus2.init(), isDinner3Favorited: FavoriteDinnerStatus3.init(), isFavorited: FavoritedStatus.init()).background(Color.init(#colorLiteral(red: 1, green: 0.3821339011, blue: 0.3108643591, alpha: 1)).ignoresSafeArea())
+            })
         }
     }
 }
+
 
 struct SearchBarPreviewView_Previews: PreviewProvider {
     static var previews: some View {
